d6afc9ee7cb5838cdc399c77fca8a4b0ae65cc21
Jay Ess <mentoce@gmail.com>
2019-05-13 14:37:45 -0700

    Remove commented out code

--- include/plt/square.h
+++ include/plt/square.h
@@ -1,6 +1,5 @@
 namespace plt
 { 
-	//template<class T> T square(T num); 
 	int square(int num);
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@ int main()
 	std::cout << "Enter a number to square: ";
 	std::cin >> num;
 
-	//int square = plt::square(num);
-
-	//std::cout << "The square of " << num << " is " << square;
 	std::cout << "The square of " << num << " is " << plt::square(num);
 
 	return 0;
